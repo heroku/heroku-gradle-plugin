6c888b229d449266cb9858b294e24a8e391d3edc
Joe Kutner <jpkutner@gmail.com>
2016-07-08 12:08:47 -0500

    Added more tests

--- src/main/groovy/com/heroku/sdk/gradle/GradleApp.groovy
+++ src/main/groovy/com/heroku/sdk/gradle/GradleApp.groovy
@@ -1,14 +1,10 @@
 package com.heroku.sdk.gradle
 
 import com.heroku.sdk.deploy.App
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 class GradleApp extends App {
 
--- src/test/groovy/com/heroku/sdk/gradle/HerokuPluginTest.groovy
+++ src/test/groovy/com/heroku/sdk/gradle/HerokuPluginTest.groovy
@@ -5,6 +5,7 @@ import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
+import org.apache.commons.io.FileUtils
 
 import org.gradle.api.internal.artifacts.mvnsettings.DefaultLocalMavenRepositoryLocator
 import org.gradle.api.internal.artifacts.mvnsettings.DefaultMavenFileLocations
@@ -24,6 +25,7 @@ class HerokuPluginTest extends Specification {
 
     File projectDir
     File buildFile
+    String appName
     List<File> pluginClasspath
 
     GradleRunner with(String... tasks) {
@@ -52,6 +54,18 @@ class HerokuPluginTest extends Specification {
     def setup() {
         projectDir = temporaryFolder.root
         buildFile = temporaryFolder.newFile('build.gradle')
+        File buildDir = temporaryFolder.newFolder('build')
+
+        FileUtils.copyFile(
+          new File("src/test/resources/sample-jar.jar"),
+          new File(buildDir, "sample-jar.jar"))
+
+        appName = "gradle-test-" + UUID.randomUUID().toString().substring(0,12);
+        if (execCond("heroku create -n ${appName}")) {
+          println("Created ${appName}")
+        } else {
+          throw RuntimeException("Failed to create app: ${appName}");
+        }
 
         def pluginClasspathResource = getClass().classLoader.findResource("plugin-classpath.txt")
         if (pluginClasspathResource == null) {
@@ -61,6 +75,10 @@ class HerokuPluginTest extends Specification {
         pluginClasspath = pluginClasspathResource.readLines()
                 .collect { it.replace('\\', '\\\\') } // escape backslashes in Windows paths
                 .collect { new File(it) }
+    }
+
+    def cleanup() {
+      println(exec("heroku destroy ${appName} --confirm ${appName}"))
     }
 
     def 'fail when missing app name'() {
@@ -75,30 +93,64 @@ class HerokuPluginTest extends Specification {
         BuildResult buildResult = with('deployHeroku').buildAndFail()
 
         then:
-        buildResult.output.contains("appName is required")
+        buildResult.output.contains("heroku.appName is required")
     }
 
     def 'success on happy path'() {
         given:
-        buildFile << '''
+        buildFile << """
             plugins {
                 id 'com.heroku.sdk.heroku-gradle'
             }
 
             heroku {
-                appName 'splat'
+                appName '${ appName }'
                 processTypes(
-                    web: "java -jar myapp.jar"
+                    web: "java -jar build/sample-jar.jar"
                 )
-                buildpacks = ["heroku/jvm-common"]
             }
-        '''.stripIndent()
+        """.stripIndent()
 
         when:
         BuildResult buildResult = with('deployHeroku').build()
 
         then:
         buildResult.task(':deployHeroku').outcome == TaskOutcome.SUCCESS
-        buildResult.output.contains("foobar")
+        buildResult.output.contains("app: ${ appName }")
+        buildResult.output.contains("including: build/")
+        buildResult.output.contains("- success")
+        buildResult.output.contains("Installing OpenJDK 1.8")
+        buildResult.output.contains("Done")
+
+        exec("curl -L http://${appName}.herokuapp.com").contains("Hello from Java!")
+    }
+
+    def 'success with Procfile'() {
+        given:
+        temporaryFolder.newFile('Procfile') << """
+            web: java -jar build/sample-jar.jar
+        """.stripIndent()
+        buildFile << """
+            plugins {
+                id 'com.heroku.sdk.heroku-gradle'
+            }
+
+            heroku {
+                appName '${ appName }'
+            }
+        """.stripIndent()
+
+        when:
+        BuildResult buildResult = with('deployHeroku').build()
+
+        then:
+        buildResult.task(':deployHeroku').outcome == TaskOutcome.SUCCESS
+        buildResult.output.contains("app: ${ appName }")
+        buildResult.output.contains("including: build/")
+        buildResult.output.contains("- success")
+        buildResult.output.contains("Installing OpenJDK 1.8")
+        buildResult.output.contains("Done")
+
+        exec("curl -L http://${appName}.herokuapp.com").contains("Hello from Java!")
     }
 }
