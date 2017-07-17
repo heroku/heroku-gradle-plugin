3c2df6d38ac7cd8f2e2e60c837ac89312b49c783
Joe Kutner <jpkutner@gmail.com>
2017-07-17 10:04:12 -1000

    Update travis setup

--- etc/travis-setup.sh
+++ etc/travis-setup.sh
@@ -1,11 +1,9 @@
-if [ -n "`git config --get user.email`" ]; then
-  echo 'already set'; else `git config --global user.email 'buildpack@example.com'`
-fi
-if [ -n "`git config --get user.name`" ]; then
-  echo 'already set'; else `git config --global user.name 'BuildpackTester'`
-fi
+[ "$CI" != "true" ] && echo "Not running on CI!" && exit 1
 
-cat <<EOF > ~/.ssh/config
+git config --global user.email ${HEROKU_API_USER:-"buildpack@example.com"}
+git config --global user.name 'BuildpackTester'
+
+cat <<EOF >> ~/.ssh/config
 Host heroku.com
     StrictHostKeyChecking no
     CheckHostIP no
@@ -14,6 +12,14 @@ Host github.com
     StrictHostKeyChecking no
 EOF
 
-curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
+cat <<EOF >> ~/.netrc
+machine git.heroku.com
+  login ${HEROKU_API_USER:-"buildpack@example.com"}
+  password ${HEROKU_API_KEY:-"password"}
+EOF
+
+sudo apt-get -qq update
+sudo apt-get install software-properties-common -y
+curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 yes | heroku keys:add
